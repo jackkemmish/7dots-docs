34dd7d4b13c1994a7de1d004f7f09f658877f1dd
Jack Kemmish <jack.kemmish@7dots.co.uk>
2021-07-29 09:07:24 +0100

    :hammer: Setting up components etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,52 @@
+const path = require('path')
+
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "7DOTS Docs",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+	siteMetadata: {
+		siteUrl: 'https://7dots-docs.com',
+		title: '7DOTS Docs',
+		author: '7DOTS',
+	},
+	plugins: [
+		'gatsby-plugin-styled-components',
+		'gatsby-plugin-gatsby-cloud',
+		'gatsby-plugin-image',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: '7dots-docs',
+				short_name: 'docs',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				icon: 'src/images/icon.png',
+			},
+		},
+		{
+			resolve: `gatsby-plugin-mdx`,
+			options: {
+				defaultLayouts: { default: path.resolve('./src/components/layout.js') },
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images/`,
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'pages',
+				path: `${__dirname}/src/pages/`,
+			},
+			__key: 'pages',
+		},
+	],
+}
---
+++ src/globalStyles.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: inherit;
+    }
+
+    *:before {
+        box-sizing: inherit;
+    }
+
+    *:after {
+        box-sizing: inherit;
+    }
+
+    html {
+        font-family: sans-serif;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        font: 112.5%/1.45em helvetica, serif;
+        box-sizing: border-box;
+        overflow-y: scroll;
+    }
+
+    body {
+        margin: 0;
+        color: hsla(0, 0%, 0%, 0.8);
+        font-weight: normal;
+        word-wrap: break-word;
+        font-kerning: normal;
+        font-feature-settings: "kern", "liga", "clig", "calt";
+    }
+`
+
+export default GlobalStyle
